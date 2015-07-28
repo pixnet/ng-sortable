72fd8340b9732276a595fa0daf6256e476b38809
yvonneyu <yvonneyu@pixnet.tw>
2015-07-28 17:35:11 +0800

    updated dist

--- dist/ng-sortable.js
+++ dist/ng-sortable.js
@@ -382,16 +382,19 @@
         $scope.removeList = []; // empty select remove item list
         // find all as-sortable-item all children
         var children = $scope.getAllSelectSortableItem(),
-          selectIndex = [];
+          selectIndex = [], selectElem;
         for (var i = 0; i < children.length; i++) {
-          if (angular.element(children[i]).scope().isSelect()) {
+          selectElem = angular.element(children[i]);
+          if (selectElem.data('_scope').isSelect()) {
             selectIndex.push(i);
+            // show hidden children element
+            selectElem.show();
           }
         }
         // reverse order to splice list will not mess up array order
         for (var j = selectIndex.length - 1; j >= 0; j--) {
           // because we are reverse order to get item, push item in front of list
-          $scope.removeList.unshift($scope.modelValue.splice(selectIndex[i], 1)[0]);
+          $scope.removeList.unshift($scope.modelValue.splice(selectIndex[j], 1)[0]);
         }
         return $scope.removeList;
       } else {
@@ -447,7 +450,7 @@
       // find as-sortable-item all children
       var children = $scope.getAllSelectSortableItem();
       for (var i = 0; i < children.length; i++) {
-        var childrenScope = angular.element(children[i]).scope();
+        var childrenScope = angular.element(children[i]).data('_scope');
         childrenScope.removeSelectClass();
         childrenScope.removeShiftSelectClass();
       }
@@ -873,8 +876,8 @@
                 // hide selectItems display
                 selectItem = angular.element(sortableItemList[i]);
                 // hide other select item
-                if (selectItem.scope().isSelect()) {
-                  selectItem.css('display', 'none');
+                if (fetchScope(selectItem).isSelect()) {
+                  selectItem.hide();
                   selectItemsLen = selectItemsLen + 1;
                 }
               }
@@ -916,6 +919,14 @@
                 isDraggable = false;
               }
               elementClicked = elementClicked.parent();
+            }
+
+            /**
+             * For multiple selection
+             * if in multiple mode, the element must been selected
+             * */
+            if (scope.sortableScope.isMultipleSelect && !sourceScope.isSelect()) {
+              isDraggable = false;
             }
             return isDraggable;
           };
@@ -1084,6 +1095,10 @@
             dragHandled = false;
             containment.css('cursor', '');
             containment.removeClass('as-sortable-un-selectable');
+            // for multiple selection, enable mouse selection
+            if (scope.sortableScope.isMultipleSelect) {
+              sortableMultiHelper.stopDrag();
+            }
           }
 
           /**
@@ -1289,9 +1304,9 @@
               if (e.ctrlKey || e.metaKey) {
                 element.toggleClass(sortableConfig.selectClass);
               } else if (e.shiftKey && shiftFlag.length) {
-                var shiftElementScope = shiftFlag.scope();
+                var shiftElementScope = shiftFlag.data('_scope');
                 if (shiftElementScope) {
-                  var elementIndex = element.scope().index();
+                  var elementIndex = element.data('_scope').index();
                   parentScope.addSelectBetweenSortableItems(shiftElementScope.index(), elementIndex);
                   parentScope.lastShiftModifyItemIndex = elementIndex;
                   return;
@@ -1318,3 +1333,153 @@
     }]);
 
 }());
+
+/*jshint indent: 2 */
+/*global angular: false */
+
+(function () {
+
+  'use strict';
+
+  var mainModule = angular.module('as.sortable');
+
+  /**
+   * Helper factory for multi selection.
+   * http://codepen.io/netgfx/pen/twAfG?editors=001
+   * http://nightlycoding.com/index.php/2014/02/click-and-drag-multi-selection-rectangle-with-javascript/
+   */
+  mainModule.factory('as.sortable.sortableMultiHelper', ['$document', 'sortableConfig',
+      function ($document, sortableConfig) {
+        var helper = {},
+          initialW = 0,
+          initialH = 0,
+          isDragging = false,
+          isInitialize = false;
+
+        helper.startDrag = function () {
+          isDragging = true;
+          cleanLocalSelection();
+        };
+
+        helper.stopDrag = function () {
+          isDragging = false;
+        };
+
+        helper.init = function () {
+
+          if (!isInitialize) {
+            isInitialize = true;
+            var dropArea = '<div class="'+ sortableConfig.mouseSelectionAreaClass +'">' +
+              '<div class="' + sortableConfig.mouseSelectionSelectionClass + '"><span></span></div></div>';
+            angular.element('body').append(dropArea);
+          }
+
+          angular.element($document).bind('mousedown.multiMouseSelection', function (event) {
+
+            if (isDragging) {
+              return;
+            }
+
+            initialW = event.pageX;
+            initialH = event.pageY;
+
+            getAngularElementByClass(sortableConfig.mouseSelectionSelectionClass)
+              .addClass(sortableConfig.mouseSelectionActiveClass);
+
+            angular.element($document).bind('mouseup.multiMouseSelection', selectElements);
+            angular.element($document).bind('mousemove.multiMouseSelection', openSelector);
+          });
+        };
+
+        helper.cleanSelection = function () {
+          angular.element($document).unbind('mousedown.multiMouseSelection');
+          getAngularElementByClass(sortableConfig.mouseSelectionAreaClass).remove();
+          isInitialize = false;
+          cleanLocalSelection();
+        };
+
+        function getAngularElementByClass (className) {
+          return angular.element(document.getElementsByClassName(className));
+        }
+
+        function selectElements() {
+          var aElem = getAngularElementByClass(sortableConfig.mouseSelectionSelectionClass);
+
+          getAngularElementByClass(sortableConfig.itemClass).each(function () {
+            var bElem = angular.element(this);
+            var result = doObjectsCollide(aElem, bElem);
+
+            if (result) {
+              bElem.addClass(sortableConfig.selectClass);
+            }
+          });
+          cleanLocalSelection();
+        }
+
+        function cleanLocalSelection() {
+          var elem = getAngularElementByClass(sortableConfig.mouseSelectionSelectionClass);
+          elem.removeClass(sortableConfig.mouseSelectionActiveClass);
+          elem.css({
+            'width': 0,
+            'height': 0
+          });
+
+          angular.element($document).unbind('mousemove.multiMouseSelection', openSelector);
+          angular.element($document).unbind('mouseup.multiMouseSelection', selectElements);
+        }
+
+        function openSelector(event) {
+
+          var w = Math.abs(initialW - event.pageX);
+          var h = Math.abs(initialH - event.pageY);
+          var selectAreaElement = getAngularElementByClass(sortableConfig.mouseSelectionSelectionClass);
+
+          selectAreaElement.css({
+            'width': w,
+            'height': h,
+            'top': initialH,
+            'left': initialW
+          });
+          if (event.pageX <= initialW && event.pageY >= initialH) {
+            selectAreaElement.css({
+              'left': event.pageX
+            });
+          } else if (event.pageY <= initialH && event.pageX >= initialW) {
+            selectAreaElement.css({
+              'top': event.pageY
+            });
+          } else if (event.pageY < initialH && event.pageX < initialW) {
+            selectAreaElement.css({
+              'left': event.pageX,
+              'top': event.pageY
+            });
+          }
+        }
+
+        function doObjectsCollide(a, b) {
+
+          var aTop = a.offset().top;
+          var aLeft = a.offset().left;
+          var bTop = b.offset().top;
+          var bLeft = b.offset().left;
+          var aHeight = a.height();
+          var aWidth = a.width();
+
+          // return if selection width and height is not valid
+          if (0 === aWidth || 0 === aHeight) {
+            return false;
+          }
+
+          return !(
+            ((aTop + aHeight) < (bTop)) ||
+            (aTop > (bTop + b.height())) ||
+            ((aLeft + aWidth) < bLeft) ||
+            (aLeft > (bLeft + b.width()))
+            );
+        }
+        return helper;
+
+      }]
+  );
+
+})();
